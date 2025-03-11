df8f348862a79813ebf73b3b44640759cb2f3979
Dominic <dominicraven2000@gmail.com>
2025-03-11 21:55:35 +0000

    Add files via upload

---
+++ JavaScript_Algorithms_and_Data_Structures/build-a-palindrome-checker.js
@@ -0,0 +1,56 @@
+// initialise constants
+const text = document.querySelector("#text-input");
+const button = document.querySelector('#check-btn');
+const resultMessage = document.querySelector('#result');
+const errorMessage = document.querySelector('#error-message');
+
+// Read text input from document
+const textInput = document.getElementById("text-input");
+
+// define function to check if value blank.
+const isBlank = (inputValue) => {
+    if (inputValue === "") {
+      console.log("Please insert value");
+      resultMessage.style.display="none";
+      errorMessage.style.display="block";
+      errorMessage.textContent = `Error: Input cannot be empty!`;
+      alert("Please input a value");
+      return true
+    } else {
+      console.log("Value Detected");
+      console.log(inputValue);
+      resultMessage.style.display="block";
+      errorMessage.style.display="none";
+      return false
+    }
+  };
+
+function cleanInputString(str) {
+  const regex = /[(),._+:/-\s]/g;
+  return str.replace(regex, '');
+}
+
+
+const isPalindrome = (inputValue) =>{
+  let cleanInput = cleanInputString(inputValue).toLowerCase();
+  const reversedInput = cleanInput.split('').reverse().join('');
+  console.log(reversedInput);
+
+
+  if (isBlank(inputValue)) {
+  } else if (cleanInput !== reversedInput) {
+    resultMessage.textContent = `${inputValue} is not a palindrome`;
+    resultMessage.style.backgroundColor = "var(--wrong-colour)";
+    resultMessage.style.color = "var(--wrong-text-colour)";
+  } else {
+    resultMessage.textContent = `${inputValue} is  a palindrome`;
+    resultMessage.style.backgroundColor = "var(--correct-colour)";
+    resultMessage.style.color = "var(--correct-text-colour)";
+  }
+};
+
+
+button.addEventListener("click", () => {
+  isPalindrome(textInput.value);
+});
+
